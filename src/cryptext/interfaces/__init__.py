f530dcec64a37051a850e441e4baddf598e5a515
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-04-26 21:08:23 +0200

    Draft of file interface

---
+++ src/cryptext/interfaces/__init__.py
@@ -0,0 +1,28 @@
+from .file_interface import PathFilterFunction, InvalidFileError
+from .file_interface import (
+    is_dir,
+    is_file,
+    exists,
+    join,
+    list_dir,
+    get_file_base_name,
+    create_file,
+    remove_file,
+    read_binary_file,
+    write_to_binary_file,
+)
+
+__all__ = [
+    'PathFilterFunction',
+    'InvalidFileError',
+    'is_dir',
+    'is_file',
+    'exists',
+    'join',
+    'list_dir',
+    'get_file_base_name',
+    'create_file',
+    'remove_file',
+    'read_binary_file',
+    'write_to_binary_file',
+]
---
+++ src/cryptext/interfaces/file_interface.py
@@ -0,0 +1,84 @@
+"""File interface. Handles read/write, creation and deletion of files."""
+
+import os
+from typing import Callable, Optional
+
+
+PathFilterFunction = Callable[[str], bool]
+
+
+# Read-only interface
+
+
+def is_dir(path: str) -> bool:
+    """Tell if a file is a directory."""
+    return os.path.isdir(path)
+
+
+def is_file(path: str) -> bool:
+    """Tell if a path corresponds to an existing file."""
+    return os.path.isfile(path)
+
+
+def exists(path: str) -> bool:
+    """Tell if a path exists."""
+    return os.path.exists(path)
+
+
+def join(*args: str) -> str:
+    """Join path elemnts to give a full valid path."""
+    return os.path.join(*args)
+
+
+def list_dir(
+    path: str, file_filter: Optional[PathFilterFunction]
+) -> list[str]:
+    """List files using an optional filter."""
+    return [
+        os.path.join(path, dir)
+        for dir in os.listdir(path)
+        if not file_filter or file_filter(os.path.join(path, dir))
+    ]
+
+
+def get_file_base_name(file: str) -> str:
+    """Return a file base name (without path)."""
+    return os.path.basename(file)
+
+
+# File modification interface
+
+
+class InvalidFileError(Exception):
+    """Raised when a file is not found."""
+
+
+def create_file(file_name: str) -> bool:
+    """Creates an empty file."""
+    with open(file_name, 'w') as file:
+        if not file:
+            raise InvalidFileError(f'File {file_name!r} not found.')
+
+
+def remove_file(file_name: str) -> None:
+    """Remove a file."""
+    if not is_file(file_name):
+        raise (FileNotFoundError(f'File {file_name!r} does not exist.'))
+
+
+def read_binary_file(file_name: str) -> bytes:
+    """Read a binary file."""
+    with open(file_name, 'rb') as file:
+        if not file:
+            raise InvalidFileError(f'File {file_name!r} not found.')
+        return file.read()
+
+
+def write_to_binary_file(file_name: str, content: bytes, append: bool) -> None:
+    """Write bytes to a binary file."""
+    mode = 'a' if append else 'w'
+    mode += 'b'
+    with open(file_name, mode) as file:
+        if not file:
+            raise InvalidFileError(f'File {file_name!r} not found.')
+        file.write(content)
